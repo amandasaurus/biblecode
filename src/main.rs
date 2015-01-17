01d9851e9dc1b014fe0a7ece04d7e828818132ce
Rory McCann <rory@technomancy.org>
2015-01-17 11:45:37 +0100

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,65 @@
-use std::num::Int;
+#[macro_use] extern crate log;
+
+use std::num::{Int, FromPrimitive};
 use std::os;
 use std::io::File;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry::{Occupied, Vacant};
 use std::iter::range_step;
 
+struct EDS {
+    needle: Vec<char>,
+    haystack: Vec<char>,
+    indexed_haystack: HashMap<char, Vec<usize>>,
+    first_pos: Vec<usize>,
+    second_pos: Vec<usize>,
+}
+
+
+impl Iterator for EDS {
+    type Item = (usize, i64);
+
+    fn next(&mut self) -> Option<(usize, i64)> {
+        None
+    }
+}
+
+impl EDS {
+    fn find_first_second_pos(&mut self) {
+        self.indexed_haystack = HashMap::new();
+        for (idx, &c) in self.haystack.iter().enumerate() {
+            match self.indexed_haystack.entry(c) {
+                Vacant(entry) => { entry.insert(vec![idx]); },
+                Occupied(mut entry) => { (*entry.get_mut()).push(idx); }
+            }
+        }
+
+        match self.indexed_haystack.get(&self.needle[0]) {
+            Some(ref entries) => {
+                self.first_pos = (**entries).clone();
+            },
+            None => {
+                self.first_pos = vec![];
+                return
+            }
+        }
+
+        match self.indexed_haystack.get(&self.needle[1]) {
+            None => { self.second_pos = vec![]; }
+            Some(ref entries) => {
+                self.second_pos = (**entries).clone();
+            }
+        }
+
+    }
+}
+
+
+
 fn search(needle: String, haystack: Vec<char>) -> Vec<(usize, i64)> {
     let needle: Vec<char> = needle.chars().collect();
     let len_needle = needle.len() as i64;
+    let len_haystack: usize = haystack.len();
     let mut results : Vec<(usize, i64)> = Vec::new();
 
     let mut indexed_haystack = HashMap::new();
@@ -35,20 +87,31 @@ fn search(needle: String, haystack: Vec<char>) -> Vec<(usize, i64)> {
             second_pos = *entries;
         }
     }
-    let mut possible_steps: Vec<(usize, usize)> = Vec::new();
     for first_pos in possible_starts.iter() {
         for second_pos in second_pos.iter() {
+            debug!("first_pos {} second_pos {}", first_pos, second_pos);
             let start = *first_pos;
             let step: i64 = (*second_pos as i64) - (*first_pos as i64);
+            if step == 1 {
+                continue;
+            }
             let positions: Vec<i64> = range_step(start as i64, step*len_needle, step).collect();
-            println!("Start {} step {}", start, step);
+            debug!("Start {} step {}", start, step);
             if positions.iter().any(|&x| { x<0 }) {
                 // TODO there's probably a better maths way to do this
+                break;
+            }
+            let positions: Vec<usize> = positions.iter().map(|&x| { FromPrimitive::from_i64(x).unwrap() }).collect();
+            debug!("positions {:?}", positions);
+            if positions.len() == 0 {
+                // WTF
                 continue;
             }
 
-            println!("Range {:?}", positions);
-            if positions.iter().enumerate().all(|(needle_idx, haystack_idx)| { needle[needle_idx] == haystack[haystack_idx] } ) {
+            if positions[positions.len()-1] > len_haystack - 1 {
+                break;
+            }
+            if positions.iter().map(|&c| { &haystack[c] }).zip(needle.iter()).all(|(&a, &b)| { a == b }) {
                 println!("Found starting at {} with step of {}", start, step);
                 results.push((start, step));
             }
@@ -62,6 +125,14 @@ fn search(needle: String, haystack: Vec<char>) -> Vec<(usize, i64)> {
 fn positions<T: Int>(start: T, step: T, len: T) -> Vec<T> {
     range(Int::zero(), len).map(|i| { start + step*i } ).collect()
 }
+
+fn only_alphanumeric(input: Vec<char>) -> Vec<char> {
+    input.iter().filter(|&c| { c.is_alphabetic() }).map(|&c| { c.to_lowercase() }).collect()
+}
+
+//fn only_constants(input: Vec<char>) -> Vec<char> {
+    //input.filter(|&c| { c.is_alphabetic() && ! ( c }).collect()
+//}
 
 fn main() {
     let file_to_search = &os::args()[1];
